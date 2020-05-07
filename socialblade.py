f4d0029f9191bfd99fa66742a41c2e1fe8721d7e
kjsm0502 <kjsm0502@gmail.com>
2020-05-07 17:48:37 +0900

    edit: 3년 월별 구독자수

--- socialblade.py
+++ socialblade.py
@@ -1,4 +1,4 @@
-def socialblade(youtube_id, chart_no):
+def socialblade(youtube_id):
     """youtube_id는 유튜브url https://www.youtube.com/channel/ 혹은 https://www.youtube.com/user/ 뒤에 붙어 있는 아이디 혹은 문자열, 
        chart_no는 socialblade에 detailed statistics에 있는 javascript상에 차트 순번,
        출력값은 리스트로 차트 결과값
@@ -23,60 +23,64 @@ def socialblade(youtube_id, chart_no):
     #javascript내에 있는 데이터 위치 찾기(10번째)
     import re
     text = str(test[10])
+    sub_txt='graph-youtube-monthly-subscribers-container'
     start_txt = 'data:'
     end_txt ='navigation'
+    
+    text = text[text.index(sub_txt):]
+    text = text[text.index(start_txt)+8:text.index(end_txt)-19]
+    
+#     #시작index 찾기
+#     indice = re.finditer(start_txt,text)
+#     for i in range(chart_no-1):
+#         indice.__next__()
+#     start_index =indice.__next__().start()+8
 
-    #시작index 찾기
-    indice = re.finditer(start_txt,text)
-    for i in range(chart_no-1):
-        indice.__next__()
-    start_index =indice.__next__().start()+8
-
-    #끝index 찾기
-    indice= re.finditer(end_txt,text)
-    for i in range(chart_no-1):
-        indice.__next__()
-    end_index =indice.__next__().start()-19
+#     #끝index 찾기
+#     indice= re.finditer(end_txt,text)
+#     for i in range(chart_no-1):
+#         indice.__next__()
+#     end_index =indice.__next__().start()-19
 
     #slice
-    sb_list = text[start_index: end_index].split('],[')
+    sb_list = text.split('],[')
 
-    #값 리스트 생성
+#     #값 리스트 생성
     sb_data = []
-    if chart_no in (3,4,5,6):
-        for i in sb_list[::-1]:
-            sb_data.append(i.split(',')[1])
-    else: #1,2는 배열뒤집을 필요없음
-        for i in sb_list:
-            sb_data.append(i.split(',')[1])
-    #return sb_data
+#    if chart_no in (3,4,5,6):
+    for i in sb_list[::-1]:
+        sb_data.append(i.split(',')[1])
+#     else: #1,2는 배열뒤집을 필요없음
+#         for i in sb_list:
+#             sb_data.append(i.split(',')[1])
+#     #return sb_data
     
     #날짜 생성 준비
     import datetime
     today = datetime.datetime.today()
     date =[]
     
-    #데이터 따라서 적합한 날짜 간격생성
+#     #데이터 따라서 적합한 날짜 간격생성
         
-    if chart_no in (3,4):
-        timeframe=30
-        for i in range(len(sb_data)):
-            today = today - datetime.timedelta(days=timeframe)
-            date.append(str(today)[:10])
-    elif chart_no in (5,6):
-        timeframe=8
-        for i in range(len(sb_data)):
-            date.append(str(today)[:10])
-            today = today - datetime.timedelta(days=timeframe)
-    else: #1,2일때
-        timeframe=7
-        for i in range(len(sb_data)):
-            date.append(str(today)[:10])
-            today = today - datetime.timedelta(days=timeframe)        
+#     if chart_no in (3,4):
+    timeframe=30
+    for i in range(len(sb_data)):
+        today = today - datetime.timedelta(days=timeframe)
+        date.append(str(today)[:10])
+#     elif chart_no in (5,6):
+#         timeframe=8
+#         for i in range(len(sb_data)):
+#             date.append(str(today)[:10])
+#             today = today - datetime.timedelta(days=timeframe)
+#     else: #1,2일때
+#         timeframe=7
+#         for i in range(len(sb_data)):
+#             date.append(str(today)[:10])
+#             today = today - datetime.timedelta(days=timeframe)        
             
     #날짜 데이터 합치기    
     return(list(zip(reversed(date),sb_data)))
 
 if __name__=='__main__':
-    a = socialblade('UCbFzvzDu17eDZ3RIeaLRswQ',4)
+    a = socialblade('UC78PMQprrZTbU0IlMDsYZPw')
     print(a)
