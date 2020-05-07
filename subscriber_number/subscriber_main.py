8844c9f31dd0e3a88c7f3a0ec4e68c30ee586b63
kjsm0502 <64782544+kjsm0502@users.noreply.github.com>
2020-05-07 22:06:32 +0900

    구독자통계 폴더 생성

--- subscriber_number/subscriber_main.py
+++ subscriber_number/subscriber_main.py
@@ -11,4 +11,4 @@ def subscriber_main(url):
 #테스트용코드 import로 다른 파일에서 실행시에는 실행되지 않음
 if __name__=='__main__':
     url='https://www.youtube.com/watch?v=zi62bIm6jCw'
-    print(subscriber_main(url))
+    print(subscriber_main(url) )
