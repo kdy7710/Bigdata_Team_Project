7c1cdcb380f8cd4e03e5708c6e3fff7445c15692
kjsm0502 <64782544+kjsm0502@users.noreply.github.com>
2020-06-14 20:37:30 +0900

    update

--- 뷰티인플루언서 영향력 유튜브/youtube_url_main.py
+++ 뷰티인플루언서 영향력 유튜브/youtube_url_main.py
@@ -6,5 +6,5 @@ def youtube_url_main(keyword, howmany):
     videolist = get_url(keyword,howmany)
     print(videolist)
 
-videolist = get_url('파이썬',30)
+videolist = get_url('파이썬',3)
 print(videolist)
