51df8be26190176c03554211dc5d9a909deb80bd
leejungjun961226 <64775844+leejungjun961226@users.noreply.github.com>
2020-06-25 16:58:31 +0900

    upadte

--- 뷰티인플루언서 영향력 유튜브/youtube_url_project.py
+++ 뷰티인플루언서 영향력 유튜브/youtube_url_project.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import re
 def get_url(plusURL, count):
     chrome_options = webdriver.ChromeOptions()
 
@@ -45,10 +49,41 @@ def get_url(plusURL, count):
     soup = BeautifulSoup(html, 'html.parser')
     videos = soup.find_all('a', class_ = 'yt-simple-endpoint style-scope ytd-video-renderer' )
 
+
     videolist = []
     baseURL = 'https://www.youtube.com'
     for i in range(0,int(count)):
         href = videos[i].attrs['href']
+        # click_count = videos[i].attrs['aria-label']
+        # click_count = re.sub('[^0-9]','',click_count.split()[-1])
         vidurl = baseURL+href
         videolist.append(vidurl)
-    return(videolist)
+
+    
+    all_list = []
+
+    for url in videolist:
+        # 내가 수정
+        one = []
+        driver.get(url)
+        #print('1')
+        elem = WebDriverWait(driver,20).until(\
+            EC.presence_of_element_located((By.CSS_SELECTOR, \
+            '#date > yt-formatted-string')))
+        #print('12')
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        notices = soup.select('#date > yt-formatted-string')
+        upload_date = notices[0].text #동영상 올린날짜 string
+        view_count = soup.select('#count > yt-view-count-renderer > span.view-count.style-scope.yt-view-count-renderer')
+        view_count = view_count[0].text
+        view_count = re.sub('[^0-9]','',view_count.split()[-1])
+        one.append(upload_date)
+        one.append(view_count)
+        all_list.append(one)
+    return all_list
+        
+
+if __name__=='__main__':
+    
+    print(get_url('달고나',5))
